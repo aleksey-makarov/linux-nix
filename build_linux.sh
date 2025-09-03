e70d29823388b4ad7ba381bc145e1c06785e6f0c
Aleksei Makarov <aleksey.makarov@gmail.com>
2025-09-03 12:07:27 +0200

    build_linux.sh: Rework starting the build

--- build_linux.sh
+++ build_linux.sh
@@ -1,35 +1,43 @@
 #!/usr/bin/env bash
 
 function build_linux() {
-    local name=$1
-    local source_git=$2
-    local version=$3
-    local config=$4
+	local name=$1
+	local source_git=$2
+	local version=$3
+	local config=$4
 
-    local LINUX_DIR DATE BUILD_DIR MODULES_DIR
-    LINUX_DIR=$(realpath "linux.$version")
-    DATE=$(date '+%y%m%d%H%M%S')
-    BUILD_DIR=$(realpath "${name}_build.$DATE")
-    MODULES_DIR=$(realpath "${name}_modules.$DATE")
+	local LINUX_DIR DATE BUILD_DIR MODULES_DIR
+	LINUX_DIR=$(realpath "linux.$version")
+	DATE=$(date '+%y%m%d%H%M%S')
+	BUILD_DIR=$(realpath "${name}_build.$DATE")
+	MODULES_DIR=$(realpath "${name}_modules.$DATE")
 
-    mkdir -p "$BUILD_DIR"
-    ln -fs -T "$BUILD_DIR" "${name}_build"
+	mkdir -p "$BUILD_DIR"
+	ln -fs -T "$BUILD_DIR" "${name}_build"
 
-    mkdir -p "$MODULES_DIR"
-    ln -fs -T "$MODULES_DIR" "${name}_modules"
+	mkdir -p "$MODULES_DIR"
+	ln -fs -T "$MODULES_DIR" "${name}_modules"
 
-    if [ ! -e "$LINUX_DIR" ] ; then
-        echo "Creating $LINUX_DIR"
-        git -c advice.detachedHead=false clone --depth=1 --branch "$version" --single-branch "file://$source_git" "$LINUX_DIR"
-    else
-        echo "Using existing linux sources from $LINUX_DIR"
-    fi
+	if [ ! -e "$LINUX_DIR" ] ; then
+		echo "Creating $LINUX_DIR"
+		git -c advice.detachedHead=false clone --depth=1 --branch "$version" --single-branch "file://$source_git" "$LINUX_DIR"
+	else
+		echo "Using existing linux sources from $LINUX_DIR"
+	fi
 
-    cp "$config" "$BUILD_DIR"/.config
-    cd "$LINUX_DIR" || exit 1
-    make O="$BUILD_DIR" olddefconfig
-    cd "$BUILD_DIR" || exit 1
-    make -j "$(nproc)"
-    export INSTALL_MOD_PATH="$MODULES_DIR"
-    make -j "$(nproc)" modules_install
+	cp "$config" "$BUILD_DIR"/.config
+	cd "$LINUX_DIR" || exit 1
+	make O="$BUILD_DIR" olddefconfig
+	cd "$BUILD_DIR" || exit 1
+
+	cat <<-EOF > go.sh
+	#!/usr/bin/env bash
+
+	make -j "\$(nproc)"
+	INSTALL_MOD_PATH="$MODULES_DIR" make -j "\$(nproc)" modules_install
+	EOF
+
+	chmod +x go.sh
+	# shellcheck source=/dev/null
+	. ./go.sh
 }
--- build_linux_vanilla.sh
+++ build_linux_vanilla.sh
@@ -2,13 +2,6 @@
 
 . ./build_linux.sh
 
-case $(uname -n) in
-	potato)
-		LINUX_MIRROR_DIR="/home/amakarov/src/linux.git"
-		;;
-	*)
-		LINUX_MIRROR_DIR="/local/mnt/workspace/amakarov/src/linux.git"
-		;;
-esac
+LINUX_MIRROR_DIR="/home/amakarov/src/linux.git"
 
 build_linux linux ${LINUX_MIRROR_DIR} v6.12.40 ./linux-config

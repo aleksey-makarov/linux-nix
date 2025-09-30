86eccb5253f0d7a9aaa8ccaf10d4158e82439c17
Aleksei Makarov <aleksey.makarov@gmail.com>
2025-09-30 22:40:06 +0300

    vulkan qemu: Add fonts

--- configuration-test.nix
+++ configuration-test.nix
@@ -65,7 +65,17 @@ with lib;
     services.openssh.enable = true;
     services.openssh.settings.PermitRootLogin = "yes";
 
-    # fonts.packages = with pkgs; [noto-fonts];
+    fonts.packages = with pkgs; [
+      # noto-fonts
+      # noto-fonts-cjk
+      # noto-fonts-emoji
+      # liberation_ttf
+      fira-code
+      fira-code-symbols
+      # mplus-outline-fonts.githubRelease
+      # dina-font
+      # proggyfonts
+    ];
 
     environment = with pkgs; {
       systemPackages = [
